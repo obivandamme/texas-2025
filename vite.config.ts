283cdca679fedd0afd872349dd8f219027b3b8ab
Pascal Martin <pascal.martin@phocassoftware.com>
2024-08-26 10:39:46 +1200

    change base

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/texas-2025',
   plugins: [react()],
   build: {
     outDir: 'dist'
